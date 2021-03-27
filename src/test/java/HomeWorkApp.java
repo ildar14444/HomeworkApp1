6d3121dab7097c842997e3a7fa91d7ea41890537
ildar14444 <ildar14444@mail.ru>
2021-03-27 16:24:10 +0300

    Домашка JAVA

---
+++ src/test/java/HomeWorkApp.java
@@ -0,0 +1,62 @@
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+            }
+
+        void printThreeWords (String[]args){
+            System.out.println(
+                    "_Orange" +
+                            "_Banana" +
+                            "_Apple"
+            )
+        }
+
+
+    void checkSumSign(String[] args) {
+        int a = 5;
+        int b = 2;
+        int c = a + b;
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    void printColor(String[] args) {
+        int value = 2;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        }
+        if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    void compareNumbers(String[] args) {
+        int a = 2;
+        int b = 3;
+
+        if (a >= b) {
+            System.out.println("a >=b"); }
+        else {
+            System.out.println("a < b"); }
+        }
+
+}
+
+
+
+
+
+
+
+
+
+
